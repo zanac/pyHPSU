7e572c43a07dd15277626395b2e43103a283089e
zanac <straccaganasse@gmail.com>
2016-12-29 14:21:33 +0100

    Add files via upload

---
+++ plugins/cloud.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import configparser, requests, sys
+
+class Cloud():
+    def get_with_default(self, config, section, name, default):
+        if "config" not in config.sections():
+            return default
+        
+        if config.has_option(section,name):
+            return config.get(section,name)
+        else:
+            return default
+        
+    def __init__(self, plugin=None):
+        self.plugin = plugin
+
+        config = configparser.ConfigParser()
+        config.read('%s.ini' % plugin)
+        self.apikey = self.get_with_default(config=config, section="config", name="apikey", default=None)
+        self.proxy = self.get_with_default(config=config, section="network", name="proxy", default=None)
+        if self.proxy:
+            self.proxy = {"http": self.proxy, "https": self.proxy }
+        
+        self.listNodes = []
+        for section in config.sections():
+            sectionName = section.lower()
+            if sectionName[0:5] == "node_":
+                dictPlugin = {}
+                for (hpsuName, cloudName) in config.items(sectionName):
+                    dictPlugin.update({hpsuName:cloudName})
+                self.listNodes.append({"name":sectionName, "data":dictPlugin})
+
+    def pushValues(self, vars):
+        if self.plugin == "EMONCMS":
+            timestamp = None
+            
+            for node in self.listNodes:
+                nodeName = node["name"][5:]
+                dictPlugin = node["data"]
+                varsDict = {}
+                for r in vars:
+                    if not timestamp:
+                        timestamp = r["timestamp"]
+                    
+                    hpsuName = r["name"]
+                    cloudName = dictPlugin[hpsuName] if hpsuName in dictPlugin else None                
+
+                    if cloudName:
+                        varsDict.update({cloudName:r["resp"]})
+
+                if len(varsDict) > 0:
+                    varsTxt = str(varsDict).replace(" ", "")
+                    _url = "https://emoncms.org/api/post?apikey=%s&time:%s&json=%s&node=%s" % (self.apikey, timestamp, varsTxt, nodeName)                
+                    
+                    r = requests.get(_url, proxies=self.proxy)
+                    
+                    rc = r.text
+                    if rc != "ok":
+                        print ("Error pushing %s" % _url)
+                    else:
+                        print ("Success push:%s" % _url)
+                
+            return True
+
+        if self.plugin == "DOMON":
+            print ("tbd")
+
