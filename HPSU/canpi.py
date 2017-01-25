dfd40d9326a021cffe86c63350d04fec46e3d3e5
zanac <straccaganasse@gmail.com>
2017-01-25 15:35:59 +0100

    Add files via upload

--- HPSU/canpi.py
+++ HPSU/canpi.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 # v 0.0.1 by Vanni Brutto (Zanac)
 
-import serial, sys, getopt, time
-import can
+import serial, sys, getopt, time, can
 
 class CanPI(object):
     def __init__(self):
@@ -16,11 +15,19 @@ class CanPI(object):
         msg = can.Message(arbitration_id=receiver_id, data=msg_data)
         self.bus.send(msg)
         time.sleep(50.0 / 1000.0)
-        timeout = 2
+        timeout = 0.2
         rcBUS = self.bus.recv(timeout)
         #print ("::::::::::::::::::::::::::")
         print (str(rcBUS.data))
+        print ("byte 0:%­s:" % (str(rcBUS.data[0])))
+        print ("byte 1:%­s:" % (str(rcBUS.data[1])))
+        print ("byte 2:%­s:" % (str(rcBUS.data[2])))
+        print ("byte 3:%­s:" % (str(rcBUS.data[3])))
         rc = " ".join("{:02X}".format(ord(c)) for c in str(rcBUS.data))
+        print("str:%­s:" % rc)
+        #todo convertire rc in stringa hex        
+        print ("::::::::::::::::::::::::::")
+        print (str(rc))
         #todo convertire rc in stringa hex
         
         return rc
