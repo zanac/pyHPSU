1de4b6486ae58b021e8fe822333853d3996771b1
zanac <straccaganasse@gmail.com>
2017-01-26 10:06:19 +0100

    Add files via upload

--- HPSU/canpi.py
+++ HPSU/canpi.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 # v 0.0.1 by Vanni Brutto (Zanac)
 
-import serial, sys, getopt, time, can
+import sys, getopt, time
+import can
 
 class CanPI(object):
     def __init__(self):
@@ -10,24 +11,31 @@ class CanPI(object):
     
     def sendCommandWithID(self, cmd):
         receiver_id = int(cmd["receiver_id"], 16)
-        msg_data = [int(r, 16) for r in cmd["command"].split(" ")]
         
-        msg = can.Message(arbitration_id=receiver_id, data=msg_data)
-        self.bus.send(msg)
-        time.sleep(50.0 / 1000.0)
-        timeout = 0.2
-        rcBUS = self.bus.recv(timeout)
-        #print ("::::::::::::::::::::::::::")
-        print (str(rcBUS.data))
-        print ("byte 0:%­s:" % (str(rcBUS.data[0])))
-        print ("byte 1:%­s:" % (str(rcBUS.data[1])))
-        print ("byte 2:%­s:" % (str(rcBUS.data[2])))
-        print ("byte 3:%­s:" % (str(rcBUS.data[3])))
-        rc = " ".join("{:02X}".format(ord(c)) for c in str(rcBUS.data))
-        print("str:%­s:" % rc)
-        #todo convertire rc in stringa hex        
-        print ("::::::::::::::::::::::::::")
-        print (str(rc))
-        #todo convertire rc in stringa hex
+        #Provare anche togliendo il commento segutente!
+        #receiver_id = receiver_id - 0x10
+        msg_data = [int(r, 16) for r in cmd["command"].split(" ")]
+        notTimeout = True
+        i = 1
+        
+        
+        while notTimeout:
+            i += 1
+            msg = can.Message(arbitration_id=receiver_id, data=msg_data)
+            self.bus.send(msg)
+            
+            time.sleep(50.0 / 1000.0)
+            timeout = 0.1
+            rcBUS = self.bus.recv(timeout)
+            #print (str(rcBUS.data))
+            print("bus:%s:%s" % (rcBUS.arbitration_id, ("%02X %02X %02X %02X %02X %02X %02X" % (rcBUS.data[0], rcBUS.data[1], rcBUS.data[2], rcBUS.data[3], rcBUS.data[4], rcBUS.data[5], rcBUS.data[6]))))
+            
+            if rcBUS.arbitration_id in [receiver_id, receiver_id - 0x10]:
+                rc = "%02X %02X %02X %02X %02X %02X %02X" % (rcBUS.data[0], rcBUS.data[1], rcBUS.data[2], rcBUS.data[3], rcBUS.data[4], rcBUS.data[5], rcBUS.data[6])
+                notTimeout = True
+            else:
+                rc = "KO"
+                if i >= 15:
+                    notTimeout = True
         
         return rc
