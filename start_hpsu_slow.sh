42e5c5c6f989d48b83002b3abadd3c753acbdd72
EmilianoMaina <emiliano.maina@gmail.com>
2017-01-30 20:32:53 +0100

    Add files via upload

--- start_hpsu_slow.sh
+++ start_hpsu_slow.sh
@@ -3,5 +3,5 @@ sudo /sbin/ip link set can0 up type can bitrate 20000
 cd /home/pi/pyHPSU-master/HPSU
 while true; do
     python3 ../pyHPSU.py -d PYCAN -c qboh -c qchhp -c qch -c qwp -c qdhw -o CLOUD -u EMONCMS
-    sleep 300
+    sleep 293
 done
