d71be0a1a7c7ba7c4ce3432d0e41245fdbf9752b
zanac <straccaganasse@gmail.com>
2017-02-06 10:21:37 +0100

    Add files via upload

--- plugins/cloud.py
+++ plugins/cloud.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import configparser, requests, sys
+import configparser
+import requests
+import sys
+
 
 class Cloud():
-    pathCOMMANDS = ""
+    hpsu = None
     
     def get_with_default(self, config, section, name, default):
         if "config" not in config.sections():
@@ -15,58 +18,71 @@ class Cloud():
         else:
             return default
         
-    def __init__(self, plugin=None, pathCOMMANDS=None):
+    def __init__(self, plugin=None, hpsu=None, logger=None):
         self.plugin = plugin
-        self.pathCOMMANDS = pathCOMMANDS
+        self.hpsu = hpsu
+        self.logger = logger
 
         #Legge file emoncms.ini
         config = configparser.ConfigParser()
-        iniFile = '%s/%s.ini' % (self.pathCOMMANDS, plugin)
+        iniFile = '%s/%s.ini' % (self.hpsu.pathCOMMANDS, plugin)
         config.read(iniFile)
         self.apikey = self.get_with_default(config=config, section="config", name="apikey", default=None)
-        #self.proxy = self.get_with_default(config=config, section="network", name="proxy", default=None)
-        #if self.proxy:
-        #    self.proxy = {"http": self.proxy, "https": self.proxy }
+                
+        self.listNodes = {}
+        self.listCmd = []
+        options = config.options("node")
+        for option in options:
+            try:
+                self.listNodes[option] = config.get("node", option).split(',')
+                self.listCmd.extend(self.listNodes[option])
+                
+                for c in self.listNodes[option]:
+                    InCommand = True
+                    for j in self.hpsu.commands:
+                        if c == j["name"]:
+                            InCommand = False
+                    if InCommand:
+                        self.hpsu.printd("warning", "command %s defined in emoncms but not in extraction" % c)
+
+            except:
+                self.listNodes[option] = None
         
-        self.listNodes = []
-        for section in config.sections():
-            sectionName = section.lower()
-            if sectionName[0:5] == "node_":
-                dictPlugin = {}
-                for (hpsuName, cloudName) in config.items(sectionName):
-                    dictPlugin.update({hpsuName:cloudName})
-                self.listNodes.append({"name":sectionName, "data":dictPlugin})
+        for r in self.hpsu.commands:
+            if r["name"] not in self.listCmd:
+                self.hpsu.printd("warning", "command %s defined in extraction but not in emoncms" % r["name"])
 
     def pushValues(self, vars):
         if self.plugin == "EMONCMS":
             timestamp = None
             
             for node in self.listNodes:
-                nodeName = node["name"][5:]
-                dictPlugin = node["data"]
+                nodeName = node[5:]
+                
                 varsDict = {}
                 for r in vars:
                     if not timestamp:
                         timestamp = r["timestamp"]
-                    
-                    hpsuName = r["name"]
-                    cloudName = dictPlugin[hpsuName] if hpsuName in dictPlugin else None                
-
-                    if cloudName:
-                        varsDict.update({cloudName:r["resp"]})
-
+                                        
+                    for r2 in self.listNodes[node]:
+                        if r2 == r["name"]:
+                            varsDict.update({r["name"]:r["resp"]})
                 if len(varsDict) > 0:
                     varsTxt = str(varsDict).replace(" ", "")
-                    _url = "https://emoncms.org/api/post?apikey=%s&time:%s&json=%s&node=%s" % (self.apikey, timestamp, varsTxt, nodeName)                
+                    _url = "https://emoncms.org/api/post?apikey=%s&time:%s&json=%s&node=%s" % (self.apikey, timestamp, varsTxt, nodeName)
+                    _urlNoApi = "https://emoncms.org/api/post?apikey=%s&time:%s&json=%s&node=%s" % ('xxx', timestamp, varsTxt, nodeName)
                     
-                    #r = requests.get(_url, proxies=self.proxy)
-                    r = requests.get(_url)
+                    try:
+                        r = requests.get(_url, timeout=7)
+                        rc = r.text
+                    except requests.exceptions.Timeout, e:
+                        rc = "ko"
+                        self.hpsu.printd("exception", "Connection timeout during get %s" % _urlNoApi)
+                    except Exception:
+                        rc = "ko"
+                        self.hpsu.printd("exception", "Exception on get %s" % _urlNoApi)
                     
-                    rc = r.text
-                    if rc != "ok":
-                        print ("Error pushing %s" % _url)
-                    else:
-                        print ("Success push:%s" % _url)
+                    
                 
             return True
 
