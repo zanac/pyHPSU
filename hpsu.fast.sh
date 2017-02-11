d9ce73f15b62dbb3f125785895894de23a3fb849
EmilianoMaina <emiliano.maina@gmail.com>
2017-02-11 14:44:45 +0100

    Using Timers instead of loops

---
+++ hpsu.fast.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+python3 /home/pi/pyHPSU-master/pyHPSU.py -d PYCAN -c flow_rate -c mode -c bpv -c posmix -c t_v1 -c t_dhw1 -c t_vbh -c t_r1 -c tliq2 -c ehs -c pump -o CLOUD -u EMONCMS
---
+++ hpsu.medium.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+python3 /home/pi/pyHPSU-master/pyHPSU.py -d PYCAN -c t_hc_set -c t_dhw_set -c t_ext -c t_outdoor_ot1 -c ta2 -o CLOUD -u EMONCMS
