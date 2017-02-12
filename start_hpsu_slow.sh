d4075b36386c6f001d5ef810aebf37ecfd51c1b6
EmilianoMaina <emiliano.maina@gmail.com>
2017-02-12 08:30:11 +0100

    Delete start_hpsu_slow.sh

--- start_hpsu_slow.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-sudo /sbin/ip link set can0 up type can bitrate 20000
-cd /home/pi/pyHPSU-master/HPSU
-while true; do
-    python3 ../pyHPSU.py -d PYCAN -c qboh -c qchhp -c qch -c qwp -c qdhw -o CLOUD -u EMONCMS
-    sleep 293
-done
