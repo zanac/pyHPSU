ea342ab7ccf882cc04fc55af07d19b7332249771
EmilianoMaina <emiliano.maina@gmail.com>
2017-02-12 08:29:59 +0100

    Delete start_hpsu_fast.sh

--- start_hpsu_fast.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-sudo /sbin/ip link set can0 up type can bitrate 20000
-cd /home/pi/pyHPSU-master/HPSU
-while true; do
-    python3 ../pyHPSU.py -d PYCAN -c flow_rate -c mode -c bpv -c posmix -c t_v1 -c t_dhw1 -c t_vbh -c t_r1 -c tliq2 -c ehs -c pump -o CLOUD -u EMONCMS
-    sleep 11
-done
