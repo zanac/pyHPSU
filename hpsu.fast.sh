080028a1aa864dcf4325e3efb13234e4f1e0a9bd
EmilianoMaina <emiliano.maina@gmail.com>
2017-02-12 17:28:05 +0100

    Adeguamento a nuova versione (logging)

--- hpsu.fast.sh
+++ hpsu.fast.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python3 /home/pi/pyHPSU-master/pyHPSU.py -d PYCAN -c flow_rate -c mode -c bpv -c posmix -c t_v1 -c t_dhw1 -c t_vbh -c t_r1 -c tliq2 -c ehs -c pump -o CLOUD -u EMONCMS
+python3 /home/pi/pyHPSU-master/pyHPSU.py -d PYCAN -c flow_rate -c mode -c bpv -c posmix -c t_v1 -c t_dhw1 -c t_vbh -c t_r1 -c tliq2 -c ehs -c pump -o CLOUD -u EMONCMS -g /home/pi/hpsu.fast.log -v 1
--- hpsu.slow.sh
+++ hpsu.slow.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 sudo /sbin/ip link set can0 up type can bitrate 20000
-python3 /home/pi/pyHPSU-master/pyHPSU.py -d PYCAN -c qboh -c qchhp -c qch -c qwp -c qdhw -o CLOUD -u EMONCMS
+python3 /home/pi/pyHPSU-master/pyHPSU.py -d PYCAN -c qboh -c qchhp -c qch -c qwp -c qdhw -o CLOUD -u EMONCMS -g /home/pi/hpsu.slow.log -v 1
